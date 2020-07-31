e38773be1a4664bae39e0aa901732994af9d2dbc
longjingsong <729358033@qq.com>
2020-07-31 18:04:54 +0800

    登陆页面的一些修改

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>wo shi yige </p>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.home{
+  width: 500px;
+  .logo{
+    border-radius: 50%;
+  }
+}
+</style>
+
+<script>
+
+export default {
+}
+</script>
